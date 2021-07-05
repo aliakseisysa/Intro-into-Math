de41f933a2c53e4b76b59f4fd3bf0c3779ae5dbd
aliakseisysa <aliaksei.sysa@gmail.com>
2021-07-05 19:50:44 +0300

    Practice_5

---
+++ Practice_5/Task_2_1.py
@@ -0,0 +1,22 @@
+#Напишите код, проверяющий любую из теорем сложения или умножения вероятности
+# на примере рулетки или подбрасывания монетки.
+
+import random
+
+orel = 0
+reshka = 0
+
+for i in range(1, 1001):
+    coin = random.randint(1, 2)
+
+    if coin == 1:
+        orel += 1
+
+    elif coin == 2:
+        reshka += 1
+
+print("Орел выпал", orel/1000)
+print("Решка выпал", reshka/1000)
+
+sum = orel/1000 + reshka/1000
+print(sum)
